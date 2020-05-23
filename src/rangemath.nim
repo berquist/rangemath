8dd29b1255fa1a972db823c974f2f3402652dc00
Eric Berquist <eric.berquist@gmail.com>
2020-05-22 21:48:20 -0400

    Finish Range implementation

--- src/rangemath.nim
+++ src/rangemath.nim
@@ -3,6 +3,11 @@ import unittest
 
 type
   Bound* = enum
+    ## A possible type of boundary for a range.
+    ##
+    ## A boundary is either
+    ## - closed, meaning it includes its endpoint, or
+    ## - open, meaning it does not.
     open
     closed
 
@@ -28,7 +33,7 @@ type
   CutKind = enum
     all
     value
-  Cut[T] = object of RootObj
+  Cut[T] = object
     ## Implementation detail for the internal structure of Range instances.
     ##
     ## Represents a unique way of "cutting" a "number line" into two sections;
@@ -36,9 +41,9 @@ type
     ## all values, or above all values. With this definition, an interval can
     ## always be represented by a pair of Cut instances.
     ##
-    ## This is a Nim port of Python code, originally written by Ryan Gabbard,
-    ## which itself is a port of Guava code originally written by Kevin
-    ## Bourrillion.
+    ## This is a Nim port of Python code from
+    ## https://github.com/isi-vista/vistautils, which itself is a port of
+    ## Guava code originally written by Kevin Bourrillion.
     position: CutPosition
     case kind: CutKind
     of CutKind.all:
@@ -135,10 +140,25 @@ type
   Range*[T] = object
     ## The boundaries of a contiguous span of values.
     ##
-    ## The value must be of some type which implements `<` in a way consistent
-    ## with equality. Note this does not provide a means for iterating over
-    ## these values.
+    ## The value must be of some type which implements ``<`` in a way
+    ## consistent with equality. Note this does not provide a means for
+    ## iterating over these values.
     ##
+    ## Each end of the Range may be *bounded* or *unbounded*. If bounded,
+    ## there is an associated *endpoint* value and the range is considered
+    ## either *open* (does not include the endpoint value) or *closed* (does
+    ## include the endpoint value). With three possibilities on each side,
+    ## this yields nine basic types of ranges, enumerated below.
+    ##
+    ## (Notation: a square bracket (``[ ]``) indicates that the range is
+    ## closed on that side; a parenthesis (``( )``) means it is either open or
+    ## unbounded. The construct ``{x | statement}`` is read "the set of all x
+    ## such that statement".)
+    ##
+    ##
+    ## When both endpoints exist, the upper endpoint may not be less than the
+    ## lower. The endpoints may be equal only if at least one of the bounds is
+    ## closed:
     ##
     lowerBound: Cut[T]
     upperBound: Cut[T]
@@ -205,22 +225,109 @@ func upperBoundType*[T](r: Range[T]): Bound =
   r.upperBound.asUpperBound
 
 func isEmpty*[T](r: Range[T]): bool =
+  ## Determine if a range is empty.
+  ##
+  ## Returns ``True`` if the range is of the form ``[v..v)`` or `(v..v]`.
+  ## (This does not encompass ranges of the form ``(v..v)``, because such
+  ## ranges are invalid and can't be constructed at all.)
+  ##
+  ## Note that certain discrete ranges such as the integer range ``(3..4)``
+  ## are not considered empty, even though the contain no actual values.
   r.lowerBound == r.upperBound
 
 func contains*[T](r: Range[T], val: T): bool =
+  ## Does the range contain the given value?
   (r.lowerBound < val) and not (r.upperBound < val)
 
 func encloses*[T](left, right: Range[T]): bool =
+  ## Does the first range enclose the second range?
   left.lowerBound.compare(right.lowerBound) <= 0 and
   left.upper_bound.compare(right.upper_bound) >= 0
 
 func isConnected*[T](left, right: Range[T]): bool =
+  ## Determine if two ranges are connected.
+  ##
+  ## Returns ``true`` if there exists a (possibly empty) range which is
+  ## enclosed by both of the given ranges. For example,
+  ## - ``[2..4)`` and ``[5..7)`` are not connected
+  ## - ``[2..4)`` and ``[3..5)`` are connected, because both enclose ``[3..4)``
+  ## - ``[2..4)`` and ``[4..6)`` are connected, because both enclose the empty range ``[4..4)``
+  ##
+  ## Note that both ranges have a well-defined union and intersection (as a
+  ## single, possibly-empty range) if and only if this method returns
+  ## ``true``.
+  ##
+  ## The connectectedness relation is both reflexive and symmetric, but does
+  ## not form an equivalence relation as it is not transitive.
+  ##
+  ## Note that certain discrete ranges are not considered connected, even
+  ## though there are no elements "between them". For example, ``[3..5]`` is
+  ## not considered connected to ``[6..10]``.
   left.lowerBound <= right.upperBound and
   right.lowerBound <= left.upperBound
 
-# TODO span
-# TODO intersection
-# TODO intersects
+func span*[T](left, right: Range[T]): Range[T] =
+  ## Get the minimal range enclosing both ranges.
+  ##
+  ## For example, the span of ``[1..3]`` and ``(5..7)`` is ``[1..7)``. If the
+  ## input ranges are connected, the returned range can also be called their
+  ## union. If they are not, note that the span might contain values that are
+  ## not contained in either input range.
+  ##
+  ## Like intersection, this operation is commutative, associative and
+  ## idempotent. Unlike intersection, it is always well-defined for any two
+  ## input ranges.
+  let
+    lowerCmp = left.lowerBound.compare(right.lowerBound)
+    upperCmp = left.upperBound.compare(right.upperBound)
+  if lowerCmp <= 0 and upperCmp >= 0:
+    left
+  elif lowerCmp >= 0 and upperCmp <= 0:
+    right
+  else:
+    Range[T](lowerBound: if lowerCmp <= 0: left.lowerBound else: right.lowerBound,
+             upperBound: if upperCmp >= 0: left.upperBound else: right.upperBound)
+
+func intersection*[T](left, right: Range[T]): Range[T] =
+  ## Get the intersection of both ranges.
+  ##
+  ## Returns the maximal range enclosed by both ranges, if such a range
+  ## exists.
+  ##
+  ## For example, the intersection of ``[1..5]`` and ``(3..7)`` is
+  ## ``(3..5]``. The resulting range may be empty; for example, ``[1..5)``
+  ## intersected with ``[5..7)`` yields the empty range ``[5..5)``.
+  ##
+  ## The intersection exists if and only if the two ranges are connected. TODO
+  ## This method throws a ``ValueError`` if this does not hold true.
+  ##
+  ## The intersection operation is commutative, associative and idempotent,
+  ## and its identity element is the ``all`` range.
+  let
+    lowerCmp = left.lowerBound.compare(right.lowerBound)
+    upperCmp = left.upperBound.compare(right.upperBound)
+  if lowerCmp >= 0 and upperCmp <= 0:
+    left
+  elif lowerCmp <= 0 and upperCmp >= 0:
+    right
+  else:
+    Range[T](lowerBound: if lowerCmp >= 0: left.lowerBound else: right.lowerBound,
+             upperBound: if upperCmp <= 0: left.upperBound else: right.upperBound)
+
+func intersects*[T](left, right: Range[T]): bool =
+  ## Do the two given ranges intersect?
+  let
+    lowerCmp = left.lowerBound.compare(right.lowerBound)
+    upperCmp = left.upperBound.compare(right.upperBound)
+  if lowerCmp >= 0 and upperCmp <= 0:
+    true
+  elif lowerCmp <= 0 and upperCmp >= 0:
+    true
+  else:
+    let
+      intersectionLowerBound = if lowerCmp >= 0: left.lowerBound else: right.lowerBound
+      intersectionUpperBound = if upperCmp <= 0: left.upperBound else: right.upperBound
+    intersectionLowerBound <= intersectionUpperBound
 
 suite "Bound":
   test "flip":
--- tests/test_range.nim
+++ tests/test_range.nim
@@ -1,6 +1,9 @@
 import rangemath
 import unittest
 
+## Tests for Range.
+##
+## Almost entirely taken from Guava's Range tests (author Kevin Bourrillion)
 suite "Range":
 
   test "Open":
@@ -13,6 +16,11 @@ suite "Range":
     check: rng.upperBoundType == Bound.open
     check: not rng.isEmpty()
     check: $rng == "(4..8)"
+    # TODO
+    # with self.assertRaises(ValueError):
+    #     Range.open(8, 4)
+    # with self.assertRaises(ValueError):
+    #     Range.open(4, 4)
 
   test "Closed":
     let rng = newRangeClosed(5, 7)
@@ -24,6 +32,12 @@ suite "Range":
     check: rng.upperBoundType == Bound.closed
     check: not rng.isEmpty()
     check: $rng == "[5..7]"
+    # workaround to make sure this can be constructed
+    check: newRangeClosed(4, 4) == newRangeClosed(4, 4)
+    # TODO
+    # with self.assertRaises(ValueError):
+    #   Range.closed(8, 4)
+
 
   test "OpenClosed":
     let rng = newRangeOpenClosed(4, 7)
@@ -96,6 +110,8 @@ suite "Range":
     check: 5 notin rng
     check: high(int) notin rng
     check: not rng.hasLowerBound()
+    # TODO
+    # self.assert_unbounded_below(rng)
     check: rng.hasUpperBound()
     check: rng.upperEndpoint == 5
     check: rng.upperBoundType == Bound.open
@@ -109,6 +125,8 @@ suite "Range":
     check: 6 in rng
     check: high(int) in rng
     check: rng.hasLowerBound()
+    # TODO
+    # self.assert_unbounded_above(rng)
     check: not rng.hasUpperBound()
     check: rng.lowerEndpoint == 5
     check: rng.lowerBoundType() == Bound.open
@@ -123,6 +141,8 @@ suite "Range":
     check: high(int) in rng
     check: rng.hasLowerBound()
     check: not rng.hasUpperBound()
+    # TODO
+    # self.assert_unbounded_above(rng)
     check: rng.lowerEndpoint == 6
     check: rng.lowerBoundType() == Bound.closed
     check: not rng.isEmpty()
@@ -135,6 +155,8 @@ suite "Range":
     check: 5 notin rng
     check: high(int) notin rng
     check: not rng.hasLowerBound()
+    # TODO
+    # self.assert_unbounded_below(rng)
     check: rng.hasUpperBound()
     check: rng.upperEndpoint == 4
     check: rng.upperBoundType() == Bound.closed
@@ -147,5 +169,184 @@ suite "Range":
     check: high(int) in rng
     check: not rng.hasLowerBound()
     check: not rng.hasUpperBound()
+    # TODO
+    # self.assert_unbounded_below(rng)
+    # self.assert_unbounded_above(rng)
     check: not rng.isEmpty()
     check: $rng == "(-∞..+∞)"
+
+  test "equals":
+    check: newRangeAll[int]() == newRangeAll[int]()
+    check: newRangeGreaterThan(2) == newRangeGreaterThan(2)
+    check: newRangeOpen(1, 5) == newRangeOpen(1, 5)
+
+  test "encloses_open":
+    let rng = newRangeOpen(2, 5)
+
+    check: rng.encloses(rng)
+    check: rng.encloses(newRangeOpen(2, 4))
+    check: rng.encloses(newRangeOpen(3, 5))
+    check: rng.encloses(newRangeClosed(3, 4))
+
+    check: not rng.encloses(newRangeOpenClosed(2, 5))
+    check: not rng.encloses(newRangeClosedOpen(2, 5))
+    check: not rng.encloses(newRangeClosed(1, 4))
+    check: not rng.encloses(newRangeClosed(3, 6))
+    check: not rng.encloses(newRangeGreaterThan(3))
+    check: not rng.encloses(newRangeLessThan(3))
+    check: not rng.encloses(newRangeAtLeast(3))
+    check: not rng.encloses(newRangeAtMost(3))
+    check: not rng.encloses(newRangeAll[int]())
+
+  test "encloses_closed":
+    let rng = newRangeClosed(2, 5)
+
+    check: rng.encloses(rng)
+    check: rng.encloses(newRangeOpen(2, 5))
+    check: rng.encloses(newRangeOpenClosed(2, 5))
+    check: rng.encloses(newRangeClosedOpen(2, 5))
+    check: rng.encloses(newRangeClosed(3, 5))
+    check: rng.encloses(newRangeClosed(2, 4))
+
+    check: not rng.encloses(newRangeOpen(1, 6))
+    check: not rng.encloses(newRangeGreaterThan(3))
+    check: not rng.encloses(newRangeLessThan(3))
+    check: not rng.encloses(newRangeAtLeast(3))
+    check: not rng.encloses(newRangeAtMost(3))
+    check: not rng.encloses(newRangeAll[int]())
+
+  test "intersection_empty":
+    let rng = newRangeClosedOpen(3, 3)
+    check: rng.intersection(rng) == rng
+    # TODO
+    # with self.assertRaises(ValueError):
+    #     rng.intersection(Range.open(3, 5))
+    # with self.assertRaises(ValueError):
+    #     rng.intersection(Range.closed(0, 2))
+
+  test "intersection_def_facto_empty":
+    let rng = newRangeOpen(3, 4)
+    check: rng.intersection(rng) == rng
+    check: rng.intersection(newRangeAtMost(3)) == newRangeOpenClosed(3, 3)
+    check: rng.intersection(newRangeAtLeast(4)) == newRangeClosedOpen(4, 4)
+    # TODO
+    # with self.assertRaises(ValueError):
+    #     rng.intersection(Range.less_than(3))
+    # with self.assertRaises(ValueError):
+    #     rng.intersection(Range.greater_than(4))
+    check: newRangeClosed(3,4).intersection(newRangeGreaterThan(4)) == newRangeOpenClosed(4, 4)
+
+  test "intersection_singleton":
+    let rng = newRangeClosed(3, 3)
+    check:rng.intersection(rng) == rng
+
+    check: rng.intersection(newRangeAtMost(4)) == rng
+    check: rng.intersection(newRangeAtMost(3)) == rng
+    check: rng.intersection(newRangeAtLeast(3)) == rng
+    check: rng.intersection(newRangeAtLeast(2)) == rng
+
+    check: rng.intersection(newRangeLessThan(3)) == newRangeClosedOpen(3, 3)
+    check: rng.intersection(newRangeGreaterThan(3)) == newRangeOpenClosed(3, 3)
+
+    # TODO
+    # with self.assertRaises(ValueError):
+    #     rng.intersection(Range.at_least(4))
+    # with self.assertRaises(ValueError):
+    #     rng.intersection(Range.at_most(2))
+
+  test "intersection_general":
+    let rng = newRangeClosed(4, 8)
+    # separate
+    # TODO
+    # with self.assertRaises(ValueError):
+    #   rng.intersection(Range.closed(0, 2))
+    # adjacent below
+    check: rng.intersection(newRangeClosedOpen(2, 4)) == newRangeClosedOpen(4, 4)
+    # overlap below
+    check: rng.intersection(newRangeClosed(2, 6)) == newRangeClosed(4, 6)
+    # enclosed with same start
+    check: rng.intersection(newRangeClosed(4, 6)) == newRangeClosed(4, 6)
+    # enclosed, interior
+    check: rng.intersection(newRangeClosed(5, 7)) == newRangeClosed(5, 7)
+    # enclosed with same end
+    check: rng.intersection(newRangeClosed(6, 8)) == newRangeClosed(6, 8)
+    # equal
+    check: rng.intersection(rng) == rng
+    # enclosing with same start
+    check: rng.intersection(newRangeClosed(4, 10)) == rng
+    # enclosing with same end
+    check: rng.intersection(newRangeClosed(2, 8)) == rng
+    # enclosing, exterior
+    check: rng.intersection(newRangeClosed(2, 10)) == rng
+    # overlap above
+    check: rng.intersection(newRangeClosed(6, 10)) == newRangeClosed(6, 8)
+    # adjacent above
+    check: rng.intersection(newRangeOpenClosed(8, 10)) == newRangeOpenClosed(8, 8)
+    # TODO
+    # with self.assertRaises(ValueError):
+    #     rng.intersection(Range.closed(10, 12))
+
+  test "intersects":
+    let rng = newRangeClosed(4, 8)
+    # separate
+    check: not rng.intersects(newRangeClosed(0, 2))
+    # adjacent below
+    check: rng.intersects(newRangeClosedOpen(2, 4))
+    # overlap below
+    check: rng.intersects(newRangeClosed(2, 6))
+    # enclosed with same start
+    check: rng.intersects(newRangeClosed(4, 6))
+    # enclosed, interior
+    check: rng.intersects(newRangeClosed(5, 7))
+    # enclosed with same end
+    check: rng.intersects(newRangeClosed(6, 8))
+    # equal
+    check: rng.intersects(rng)
+    # enclosing with same start
+    check: rng.intersects(newRangeClosed(4, 10))
+    # enclosing with same end
+    check: rng.intersects(newRangeClosed(2, 8))
+    # enclosing, exterior
+    check: rng.intersects(newRangeClosed(2, 10))
+    # overlap above
+    check: rng.intersects(newRangeClosed(6, 10))
+    # adjacent above
+    check: rng.intersects(newRangeOpenClosed(8, 10))
+    check: not rng.intersects(newRangeClosed(10, 12))
+
+  test "create_spanning":
+    discard
+    # TODO
+    # with self.assertRaisesRegex(
+    #     ValueError, "Cannot create range from span of empty range collection"
+    # ):
+    #     Range.create_spanning([])
+
+  # Currently not usable in set
+  # test "check_usable_in_set":
+  #   let setOfRanges = [
+  #     newRangeOpenClosed(0, 1),
+  #     newRangeOpenClosed(0, 1),
+  #     newRangeAtMost(1),
+  #     newRangeAtMost(1)
+  #   ].toHashSet()
+  #   check: len(setOfRanges) == 2
+
+  # TODO
+  # def assert_unbounded_below(self, rng: Range):
+  #     self.assertFalse(rng.has_lower_bound())
+  #     with self.assertRaises(ValueError):
+  #         rng.lower_endpoint()
+  #     # pylint: disable=pointless-statement
+  #     with self.assertRaises(AssertionError):
+  #         rng.lower_bound_type
+
+  # TODO
+  # def assert_unbounded_above(self, rng: Range):
+  #     self.assertFalse(rng.has_upper_bound())
+  #     with self.assertRaises(ValueError):
+  #         rng.upper_endpoint()
+  #     # pylint: disable=pointless-statement
+  #     with self.assertRaises(AssertionError):
+  #         rng.upper_bound_type
+
